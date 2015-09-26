c7f6f1b5f0c549fb72fffad290c34be555d3b970
patrickkillalea <killalea.patrick@gmail.com>
2015-09-26 16:18:51 +0100

    Added s

--- db.js
+++ db.js
@@ -8,30 +8,6 @@ mongoose.connect('mongodb://patrickkillalea:ilwp2000@ds055802.mongolab.com:55802
  * Lets define our Model for User entity. This model represents a collection in the database.
  * We define the possible schema of User document and data types of each field.
  * */
-var User = mongoose.model('User', {name: String, roles: Array, age: Number});
-
-/**
- * Lets Use our Models
- * */
-
-//Lets create a new user
-var user1 = new User({name: 'modulus admin', age: 56, roles: ['admin', 'moderator', 'user']});
-
-//Some modifications in user object
-user1.name = user1.name.toUpperCase();
-
-//Lets try to print and see it. You will see _id is assigned.
-console.log(user1);
-
-//Lets save it
-user1.save(function (err, userObj) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('saved successfully:', userObj);
-  }
-});
-
 
 app.get('/', function(req, res) {
     var drinks = [
